c1a3add37c1b2edd72e9a782d42929b67c8547d6
Pablo Gómez Álvarez <pablo@MacBook-Pro-de-Pablo.local>
2021-02-17 16:50:55 +0100

    commit 25

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,28 +14,23 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n > -2147483648 || n <= 2147483647)
+	if (n == -2147483648)
 	{
-		if (n == -2147483648)
-		{
-			ft_putchar_fd('-', fd);
-			ft_putchar_fd('2', fd);
-			ft_putnbr_fd(147483648, fd);
-		}
-		else if (n >= 10)
-		{
-			ft_putnbr_fd(n / 10, fd);
-			ft_putnbr_fd(n % 10, fd);
-		}
-		else if (n < 0)
-		{
-			n *= -1;
-			ft_putchar_fd('-', fd);
-			ft_putnbr_fd(n, fd);
-		}
-		else
-		{
-			ft_putchar_fd(n + '0', fd);
-		}
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
 	}
+	else if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else if (n < 0)
+	{
+		n *= -1;
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(n, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
 }
